49d90598a048e8196c9dbbdc723e41bc4f24b3ba
jaime Escobar Saavedra <jaimescobarsaavedra@gmail.com>
2023-04-24 22:22:34 -0500

    Se realiza la correción en redirección de paginas, y de adiciona todo para ver la DB

--- InicioSesion.inc.php
+++ InicioSesion.inc.php
@@ -10,9 +10,7 @@ include_once 'APP/ControlSesion.inc.php';
 conexion::abrir_conexion();
 $Total_Usuarios = RepoUsuario::Obtener_numero_usuarios(conexion::obtener_conexion());
 conexion::cerrar_conexion();
-if (ControlSesion::Sesion_iniciada()) {
-    Redireccion::redirigir(RUTA_TRYBIT);
-}
+
 if (isset($_POST['login'])) {
     conexion::abrir_conexion();
     $validador = new ValidadorInicioSesion($_POST['correo'], $_POST['password'], conexion::obtener_conexion());
@@ -42,24 +40,24 @@ if (isset($_POST['login'])) {
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="CSS/assets/img/favicon.png" rel="icon">
-    <link href="CSS/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="../../../CSS/assets/img/favicon.png" rel="icon">
+    <link href="../../../CSS/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
     <link href="https://fonts.gstatic.com" rel="preconnect">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="CSS/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="CSS/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="CSS/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="CSS/assets/vendor/quill/quill.snow.css" rel="stylesheet">
-    <link href="CSS/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-    <link href="CSS/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-    <link href="CSS/assets/vendor/simple-datatables/style.css" rel="stylesheet">
+    <link href="../../../CSS/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../../CSS/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="../../../CSS/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="../../../CSS/assets/vendor/quill/quill.snow.css" rel="stylesheet">
+    <link href="../../../CSS/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+    <link href="../../../CSS/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="../../../CSS/assets/vendor/simple-datatables/style.css" rel="stylesheet">
 
     <!-- Template Main CSS File -->
-    <link href="CSS/assets/css/style.css" rel="stylesheet">
+    <link href="../../../CSS/assets/css/style.css" rel="stylesheet">
 
 </head>
 
@@ -73,7 +71,7 @@ if (isset($_POST['login'])) {
                         <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
 
                             <div class="d-flex justify-content-center py-4">
-                                <a href="index.php" class="logo d-flex align-items-center w-auto">
+                                <a href="../../../index.php" class="logo d-flex align-items-center w-auto">
                                     <img src="assets/img/logo.png" alt="">
                                     <span class="d-none d-lg-block">Trybit</span>
                                 </a>
@@ -151,17 +149,17 @@ if (isset($_POST['login'])) {
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
     <!-- Vendor JS Files -->
-    <script src="CSS/assets/vendor/apexcharts/apexcharts.min.js"></script>
-    <script src="CSS/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="CSS/assets/vendor/chart.js/chart.umd.js"></script>
-    <script src="CSS/assets/vendor/echarts/echarts.min.js"></script>
-    <script src="CSS/assets/vendor/quill/quill.min.js"></script>
-    <script src="CSS/assets/vendor/simple-datatables/simple-datatables.js"></script>
-    <script src="CSS/assets/vendor/tinymce/tinymce.min.js"></script>
-    <script src="CSS/assets/vendor/php-email-form/validate.js"></script>
+    <script src="../../../CSS/assets/vendor/apexcharts/apexcharts.min.js"></script>
+    <script src="../../../CSS/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../../../CSS/assets/vendor/chart.js/chart.umd.js"></script>
+    <script src="../../../CSS/assets/vendor/echarts/echarts.min.js"></script>
+    <script src="../../../CSS/assets/vendor/quill/quill.min.js"></script>
+    <script src="../../../CSS/assets/vendor/simple-datatables/simple-datatables.js"></script>
+    <script src="../../../CSS/assets/vendor/tinymce/tinymce.min.js"></script>
+    <script src="../../../CSS/assets/vendor/php-email-form/validate.js"></script>
 
     <!-- Template Main JS File -->
-    <script src="CSS/assets/js/main.js"></script>
+    <script src="../../../CSS/assets/js/main.js"></script>
 
 </body>
 
